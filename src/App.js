18f87e50289d5887974f21fae1544162c4f440e4
David Wolverton <dwolvert@gmail.com>
2021-01-23 09:49:44 -0500

    add CSS

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+// 1. Import counter component
 import Counter from "./components/Counter";
 import "./styles.css";
 
 export default function App() {
   return (
     <div className="App">
+      {/* 2. Insert Counter component */}
       <Counter />
     </div>
   );
--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./Counter.css"; // Import CSS file
 
 function Counter() {
   return (
