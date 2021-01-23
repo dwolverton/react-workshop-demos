6117db3c1bb5711907438f3e5e5f17ce2b2cc846
David Wolverton <dwolvert@gmail.com>
2021-01-23 10:45:37 -0500

    counter state

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,15 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Counter.css"; // Import CSS file
 
 function Counter() {
+  const [count, setCount] = useState(0);
+
+  function countUp() {
+    setCount(count + 1);
+  }
+  function resetCount() {
+    setCount(0);
+  }
+
   return (
+    // return the JSX that makes up this component
     <div className="Counter">
       <h3>Counter</h3>
-      <p>0</p>
-      <p>
-        <button>Down</button>
-        <button>Reset</button>
-        <button>Up</button>
+      <p>{count}</p>
+      <p className="Counter__buttons">
+        <button onClick={() => setCount(count - 1)}>Down</button>
+        <button onClick={resetCount}>Reset</button>
+        <button onClick={countUp}>Up</button>
       </p>
     </div>
   );
