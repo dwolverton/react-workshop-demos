38df550ac5b1f5d0ee5855ae78f680627a0f20eb
David Wolverton <dwolvert@gmail.com>
2021-01-23 09:42:04 -0500

    Counter

---
+++ src/components/Counter.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Counter() {
+  return (
+    <div className="Counter">
+      <h3>Counter</h3>
+      <p>0</p>
+      <p>
+        <button>Down</button>
+        <button>Reset</button>
+        <button>Up</button>
+      </p>
+    </div>
+  );
+}
+
+export default Counter;
