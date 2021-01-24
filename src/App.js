fbc4b57c914565ae9ce35e71e0bea85c31c9a654
David Wolverton <dwolvert@gmail.com>
2021-01-24 09:23:34 -0500

    Started Weather

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@
 import Counter from "./components/Counter";
 import Switch from "./components/Switch";
 import Votes from "./components/Votes";
+import Weather from "./components/Weather";
 import "./styles.css";
 
 export default function App() {
   return (
     <div className="App">
+      <Weather />
       <Switch />
       <Switch />
       <Counter />
---
+++ src/components/Weather.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Weather.css";
+
+function Weather() {
+  return (
+    <div className="Weather">
+      <h3>Weather for ___</h3>
+      <p>___conditions___</p>
+      <p>Temperature: ___F (___C)</p>
+    </div>
+  );
+}
+
+export default Weather;
